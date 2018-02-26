5b5cd648064e5c9be226d9478ff43bc821bc5ba9
T145 <gnosoman@gmail.com>
2018-02-26 14:29:04 -0500

    Fixed Spider Creeper charge rendering

--- src/main/java/T145/elementalcreepers/client/render/entity/LayerCharge.java
+++ src/main/java/T145/elementalcreepers/client/render/entity/LayerCharge.java
@@ -1,8 +1,11 @@
 package T145.elementalcreepers.client.render.entity;
 
-import T145.elementalcreepers.client.render.model.ModelSpiderCreeper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelCreeper;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderCreeper;
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.util.ResourceLocation;
@@ -10,25 +13,30 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class LayerSpiderCharge implements LayerRenderer<EntityCreeper> {
+public class LayerCharge implements LayerRenderer<EntityCreeper> {
 
 	private static final ResourceLocation LIGHTNING_TEXTURE = new ResourceLocation("textures/entity/creeper/creeper_armor.png");
-	private final RenderSpiderCreeper creeperRenderer;
-	private final ModelSpiderCreeper creeperModel = new ModelSpiderCreeper(2.0F);
+	private final RenderLiving creeperRenderer;
+	private final ModelBase creeperModel;
 
-	public LayerSpiderCharge(RenderSpiderCreeper creeperRenderer) {
+	public LayerCharge(RenderLiving creeperRenderer, ModelBase creeperModel) {
 		this.creeperRenderer = creeperRenderer;
+		this.creeperModel = creeperModel;
+	}
+
+	public LayerCharge(RenderLiving creeperRenderer) {
+		this(creeperRenderer, new ModelCreeper(2F));
 	}
 
 	@Override
-	public void doRenderLayer(EntityCreeper entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-		if (entity.getPowered()) {
-			boolean flag = entity.isInvisible();
+	public void doRenderLayer(EntityCreeper entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		if (entitylivingbaseIn.getPowered()) {
+			boolean flag = entitylivingbaseIn.isInvisible();
 			GlStateManager.depthMask(!flag);
-			creeperRenderer.bindTexture(LIGHTNING_TEXTURE);
+			this.creeperRenderer.bindTexture(LIGHTNING_TEXTURE);
 			GlStateManager.matrixMode(5890);
 			GlStateManager.loadIdentity();
-			float f = entity.ticksExisted + partialTicks;
+			float f = (float) entitylivingbaseIn.ticksExisted + partialTicks;
 			GlStateManager.translate(f * 0.01F, f * 0.01F, 0.0F);
 			GlStateManager.matrixMode(5888);
 			GlStateManager.enableBlend();
@@ -36,9 +44,9 @@ public class LayerSpiderCharge implements LayerRenderer<EntityCreeper> {
 			GlStateManager.color(0.5F, 0.5F, 0.5F, 1.0F);
 			GlStateManager.disableLighting();
 			GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
-			creeperModel.setModelAttributes(creeperRenderer.getMainModel());
+			this.creeperModel.setModelAttributes(this.creeperRenderer.getMainModel());
 			Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
-			creeperModel.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+			this.creeperModel.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
 			Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
 			GlStateManager.matrixMode(5890);
 			GlStateManager.loadIdentity();
--- src/main/java/T145/elementalcreepers/client/render/entity/RenderSpiderCreeper.java
+++ src/main/java/T145/elementalcreepers/client/render/entity/RenderSpiderCreeper.java
@@ -2,66 +2,53 @@ package T145.elementalcreepers.client.render.entity;
 
 import T145.elementalcreepers.ElementalCreepers;
 import T145.elementalcreepers.client.render.model.ModelSpiderCreeper;
-import T145.elementalcreepers.entities.base.EntityBaseCreeper;
+import T145.elementalcreepers.entities.EntitySpiderCreeper;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class RenderSpiderCreeper extends RenderLiving {
+public class RenderSpiderCreeper extends RenderLiving<EntitySpiderCreeper> {
 
 	public RenderSpiderCreeper(RenderManager renderManager) {
 		super(renderManager, new ModelSpiderCreeper(), 0.5F);
 		addLayer(new LayerFestive(this));
-		addLayer(new LayerSpiderCharge(this));
+		addLayer(new LayerCharge(this, new ModelSpiderCreeper(2F)));
 	}
 
 	@Override
-	protected void preRenderCallback(EntityLivingBase entity, float partialTickTime) {
-		if (entity instanceof EntityBaseCreeper) {
-			EntityBaseCreeper creeper = (EntityBaseCreeper) entity;
-			float f1 = creeper.getCreeperFlashIntensity(partialTickTime);
-			float f2 = 1.0F + MathHelper.sin(f1 * 100.0F) * f1 * 0.01F;
-
-			f1 = MathHelper.clamp(f1, 0.0F, 1.0F);
-			f1 *= f1;
-			f1 *= f1;
-
-			float f3 = (1.0F + f1 * 0.4F) * f2;
-			float f4 = (1.0F + f1 * 0.1F) / f2;
-
-			GlStateManager.scale(f3, f4, f3);
-		}
-
-		super.preRenderCallback(entity, partialTickTime);
+	protected void preRenderCallback(EntitySpiderCreeper creeper, float partialTickTime) {
+		float f = creeper.getCreeperFlashIntensity(partialTickTime);
+		float f1 = 1.0F + MathHelper.sin(f * 100.0F) * f * 0.01F;
+		f = MathHelper.clamp(f, 0.0F, 1.0F);
+		f = f * f;
+		f = f * f;
+		float f2 = (1.0F + f * 0.4F) * f1;
+		float f3 = (1.0F + f * 0.1F) / f1;
+		GlStateManager.scale(f2, f3, f2);
 	}
 
 	@Override
-	protected int getColorMultiplier(EntityLivingBase entity, float lightBrightness, float partialTickTime) {
-		if (entity instanceof EntityBaseCreeper) {
-			EntityBaseCreeper creeper = (EntityBaseCreeper) entity;
-			float f2 = creeper.getCreeperFlashIntensity(partialTickTime);
+	protected int getColorMultiplier(EntitySpiderCreeper creeper, float lightBrightness, float partialTickTime) {
+		float f = creeper.getCreeperFlashIntensity(partialTickTime);
 
-			if (f2 * 10.0F % 2 == 0) {
-				return 0;
-			}
-
-			int i = (int) (f2 * 0.2F * 255.0F);
+		if ((int) (f * 10.0F) % 2 == 0) {
+			return 0;
+		} else {
+			int i = (int) (f * 0.2F * 255.0F);
 			i = MathHelper.clamp(i, 0, 255);
-			return i << 24 | 0xFFFFFF;
+			return i << 24 | 822083583;
 		}
-
-		return super.getColorMultiplier(entity, lightBrightness, partialTickTime);
 	}
 
 	@Override
-	protected ResourceLocation getEntityTexture(Entity entity) {
+	protected ResourceLocation getEntityTexture(EntitySpiderCreeper entity) {
 		return new ResourceLocation(ElementalCreepers.MODID, "textures/entities/spidercreeper.png");
 	}
 }
