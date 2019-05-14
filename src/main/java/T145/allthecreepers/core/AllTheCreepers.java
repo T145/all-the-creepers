dbe179988bd7c21c5b6bef050397f284d17b0ab4
T145 <T145@protonmail.com>
2019-05-13 22:52:25 -0400

    Added Firework Creeper

--- src/main/java/T145/allthecreepers/core/AllTheCreepers.java
+++ src/main/java/T145/allthecreepers/core/AllTheCreepers.java
@@ -12,6 +12,7 @@ import T145.allthecreepers.entities.DarkCreeperEntity;
 import T145.allthecreepers.entities.DemolitionCreeperEntity;
 import T145.allthecreepers.entities.EarthCreeperEntity;
 import T145.allthecreepers.entities.FireCreeperEntity;
+import T145.allthecreepers.entities.FireworkCreeperEntity;
 import T145.allthecreepers.entities.FurnaceCreeperEntity;
 import T145.allthecreepers.entities.LavaCreeperEntity;
 import T145.allthecreepers.entities.LightningCreeperEntity;
@@ -37,18 +38,17 @@ public class AllTheCreepers implements ModInitializer, ClientModInitializer {
 	private static final EntitySize CREEPER_SIZE = EntitySize.constant(0.6F, 1.7F);
 	public static final EntityType<CakeCreeperEntity> CAKE_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.CAKE.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, CakeCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<CookieCreeperEntity> COOKIE_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.COOKIE.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, CookieCreeperEntity::new).size(CREEPER_SIZE).build());
+	public static final EntityType<DarkCreeperEntity> DARK_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.DARK.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, DarkCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<DemolitionCreeperEntity> DEMOLITION_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.DEMOLTIION.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, DemolitionCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<EarthCreeperEntity> EARTH_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.EARTH.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, EarthCreeperEntity::new).size(CREEPER_SIZE).build());
-
+	public static final EntityType<FireCreeperEntity> FIRE_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.FIRE.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, FireCreeperEntity::new).size(CREEPER_SIZE).build());
+	public static final EntityType<FireworkCreeperEntity> FIREWORK_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.FIREWORK.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, FireworkCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<FurnaceCreeperEntity> FURNACE_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.FURNACE.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, FurnaceCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<LavaCreeperEntity> LAVA_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.LAVA.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, LavaCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<LightningCreeperEntity> LIGHTNING_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.LIGHTNING.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, LightningCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<WaterCreeperEntity> WATER_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.WATER.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, WaterCreeperEntity::new).size(CREEPER_SIZE).build());
-
-	public static final EntityType<FireCreeperEntity> FIRE_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.FIRE.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, FireCreeperEntity::new).size(CREEPER_SIZE).build());
-	public static final EntityType<BallisticCreeperEntity> BALLISTIC_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.BALLISTIC.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, BallisticCreeperEntity::new).size(CREEPER_SIZE).build());
 	public static final EntityType<LuminousCreeperEntity> LUMINOUS_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.LUMINOUS.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, LuminousCreeperEntity::new).size(CREEPER_SIZE).build());
-	public static final EntityType<DarkCreeperEntity> DARK_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.DARK.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, DarkCreeperEntity::new).size(CREEPER_SIZE).build());
+	public static final EntityType<BallisticCreeperEntity> BALLISTIC_CREEPER = Registry.register(Registry.ENTITY_TYPE, CreeperType.BALLISTIC.getIdentifier(), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, BallisticCreeperEntity::new).size(CREEPER_SIZE).build());
 
 	@Override
 	public void onInitialize() {
@@ -73,5 +73,6 @@ public class AllTheCreepers implements ModInitializer, ClientModInitializer {
 		EntityRendererRegistry.INSTANCE.register(BallisticCreeperEntity.class, ((dispatcher, context) -> new ElementalCreeperRenderer(dispatcher)));
 		EntityRendererRegistry.INSTANCE.register(LuminousCreeperEntity.class, ((dispatcher, context) -> new ElementalCreeperRenderer(dispatcher)));
 		EntityRendererRegistry.INSTANCE.register(DarkCreeperEntity.class, ((dispatcher, context) -> new ElementalCreeperRenderer(dispatcher)));
+		EntityRendererRegistry.INSTANCE.register(FireworkCreeperEntity.class, ((dispatcher, context) -> new ElementalCreeperRenderer(dispatcher)));
 	}
 }
---
+++ src/main/java/T145/allthecreepers/entities/FireworkCreeperEntity.java
@@ -0,0 +1,79 @@
+package T145.allthecreepers.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import T145.allthecreepers.api.IElementalCreeper;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.FireworkEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.item.FireworkItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.util.DyeColor;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.Explosion.DestructionType;
+
+public class FireworkCreeperEntity extends CreeperEntity implements IElementalCreeper {
+
+	public FireworkCreeperEntity(EntityType<? extends CreeperEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Override
+	public String getTextureName() {
+		return "firework";
+	}
+
+	@Override
+	public boolean canDetonate() {
+		return true;
+	}
+
+	private ItemStack createFirework(int type) {
+		ItemStack firework = new ItemStack(Items.FIREWORK_ROCKET, 1);
+		ItemStack starFirework = new ItemStack(Items.FIREWORK_STAR);
+		CompoundTag starExplosionTag = starFirework.getOrCreateSubCompoundTag("Explosion");
+		List<Integer> colorTags = new ArrayList<>();
+		DyeColor color = DyeColor.byId(random.nextInt(DyeColor.values().length));
+
+		colorTags.add(color.getFireworkColor());
+		starExplosionTag.putIntArray("Colors", colorTags);
+		starExplosionTag.putByte("Type", (byte) type);
+
+		CompoundTag fireworkTag = firework.getOrCreateSubCompoundTag("Fireworks");
+		ListTag explosionTags = new ListTag();
+		CompoundTag explosionTag = starFirework.getSubCompoundTag("Explosion");
+
+		if (explosionTag != null) {
+			explosionTags.add(explosionTag);
+		}
+
+		fireworkTag.putByte("Flight", (byte) 2);
+
+		if (!explosionTags.isEmpty()) {
+			fireworkTag.put("Explosions", explosionTags);
+		}
+
+		return firework;
+	}
+
+	@Override
+	public void detonate(DestructionType destructionType, byte radius, Explosion simpleExplosion) {
+		world.spawnEntity(new FireworkEntity(world, x, y, z, createFirework(FireworkItem.Type.CREEPER.getId())));
+
+		for (LivingEntity entity : world.getEntities(LivingEntity.class, this.getAOE(radius, x, y, z), victim -> victim != this)) {
+			if (entity.isAlive() && !(entity instanceof HostileEntity)) {
+				FireworkEntity firework = new FireworkEntity(world, entity.x, entity.y, entity.z, createFirework(random.nextInt(5)));
+
+				world.spawnEntity(firework);
+				entity.startRiding(firework);
+			}
+		}
+	}
+}
