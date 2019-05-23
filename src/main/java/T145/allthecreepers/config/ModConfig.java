421f0e00c8fc0d4717e4cdc0434599b5065042c5
T145 <T145@protonmail.com>
2019-05-23 15:14:02 -0400

    Tweaked default mob spawns so they aren't as overwhelming

--- src/main/java/T145/allthecreepers/config/ModConfig.java
+++ src/main/java/T145/allthecreepers/config/ModConfig.java
@@ -13,46 +13,46 @@ public class ModConfig implements ConfigData {
 	public final boolean debug = false;
 
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int ballisticCreeperSpawnWeight = 100;
+	public final int ballisticCreeperSpawnWeight = 25;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int cakeCreeperSpawnWeight = 100;
+	public final int cakeCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int cookieCreeperSpawnWeight = 100;
+	public final int cookieCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int darkCreeperSpawnWeight = 100;
+	public final int darkCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int demolitionCreeperSpawnWeight = 100;
+	public final int demolitionCreeperSpawnWeight = 25;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int earthCreeperSpawnWeight = 100;
+	public final int earthCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int fireCreeperSpawnWeight = 100;
+	public final int fireCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int fireworkCreeperSpawnWeight = 100;
+	public final int fireworkCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int furnaceCreeperSpawnWeight = 100;
+	public final int furnaceCreeperSpawnWeight = 25;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int lavaCreeperSpawnWeight = 100;
+	public final int lavaCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int lightningCreeperSpawnWeight = 100;
+	public final int lightningCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int luminousCreeperSpawnWeight = 100;
+	public final int luminousCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int natureCreeperSpawnWeight = 100;
+	public final int natureCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int partyCreeperSpawnWeight = 100;
+	public final int partyCreeperSpawnWeight = 50;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_WEIGHT)
-	public final int waterCreeperSpawnWeight = 100;
+	public final int waterCreeperSpawnWeight = 50;
 
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
-	public final int ballisticCreeperSpawnMinGroupSize = 4;
+	public final int ballisticCreeperSpawnMinGroupSize = 2;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
-	public final int cakeCreeperSpawnMinGroupSize = 4;
+	public final int cakeCreeperSpawnMinGroupSize = 2;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
-	public final int cookieCreeperSpawnMinGroupSize = 4;
+	public final int cookieCreeperSpawnMinGroupSize = 3;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
 	public final int darkCreeperSpawnMinGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
-	public final int demolitionCreeperSpawnMinGroupSize = 4;
+	public final int demolitionCreeperSpawnMinGroupSize = 2;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
 	public final int earthCreeperSpawnMinGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
@@ -60,30 +60,30 @@ public class ModConfig implements ConfigData {
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
 	public final int fireworkCreeperSpawnMinGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
-	public final int furnaceCreeperSpawnMinGroupSize = 4;
+	public final int furnaceCreeperSpawnMinGroupSize = 2;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
 	public final int lavaCreeperSpawnMinGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
 	public final int lightningCreeperSpawnMinGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
-	public final int luminousCreeperSpawnMinGroupSize = 4;
+	public final int luminousCreeperSpawnMinGroupSize = 3;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
 	public final int natureCreeperSpawnMinGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
-	public final int partyCreeperSpawnMinGroupSize = 4;
+	public final int partyCreeperSpawnMinGroupSize = 2;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MIN_SIZE)
 	public final int waterCreeperSpawnMinGroupSize = 4;
 
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
-	public final int ballisticCreeperSpawnMaxGroupSize = 4;
+	public final int ballisticCreeperSpawnMaxGroupSize = 3;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
-	public final int cakeCreeperSpawnMaxGroupSize = 4;
+	public final int cakeCreeperSpawnMaxGroupSize = 3;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
 	public final int cookieCreeperSpawnMaxGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
 	public final int darkCreeperSpawnMaxGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
-	public final int demolitionCreeperSpawnMaxGroupSize = 4;
+	public final int demolitionCreeperSpawnMaxGroupSize = 3;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
 	public final int earthCreeperSpawnMaxGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
@@ -91,7 +91,7 @@ public class ModConfig implements ConfigData {
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
 	public final int fireworkCreeperSpawnMaxGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
-	public final int furnaceCreeperSpawnMaxGroupSize = 4;
+	public final int furnaceCreeperSpawnMaxGroupSize = 3;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
 	public final int lavaCreeperSpawnMaxGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
@@ -101,7 +101,7 @@ public class ModConfig implements ConfigData {
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
 	public final int natureCreeperSpawnMaxGroupSize = 4;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
-	public final int partyCreeperSpawnMaxGroupSize = 4;
+	public final int partyCreeperSpawnMaxGroupSize = 3;
 	@ConfigEntry.Category(RegistryATC.CATEGORY_SPAWN_MAX_SIZE)
 	public final int waterCreeperSpawnMaxGroupSize = 4;
 }
